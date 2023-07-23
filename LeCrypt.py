520903ccadc74d880c70e54d7f37d9767d683e19
Legion <74609879+legion3trunn3r@users.noreply.github.com>
2023-07-23 14:14:33 +0300

    Update LeCrypt.py

--- LeCrypt.py
+++ LeCrypt.py
@@ -15,11 +15,8 @@ password = input("Write your password to encrypt: ")
 password_again = input("Repeat your password: ")
 if password == password_again:
 
-	#try:
 	our_hash = LeCrypt.Encrypt(user_input, password)
 	print(f"\n=========================================\n\nYour hash: {our_hash} \n\n=========================================")
-	#except ValueError:
-	#	print("\n\n[*] This symbol can't be encrypted")
 
 else:
 	print("[*] Please try again")
